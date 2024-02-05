0ab7a81c530db074a180e10af0245ab4ed5cd3db
Blake Watanabe <blakewanabi@gmail.com>
2024-02-04 15:09:00 -1000

    E26 WOD

---
+++ sumofnumbers.js
@@ -0,0 +1,38 @@
+const testNumList = [1, 2, 3, 4];
+
+function sumFor(numList) {
+  let sum = 0;
+  for (const num of numList) {
+    sum += num;
+  }
+  return sum;
+}
+
+console.log(sumFor(testNumList));
+
+function sumWhile(numList) {
+  let sum = 0;
+  let i = 0;
+  while (i < numList.length) {
+    sum += numList[i];
+    i++;
+  }
+  return sum;
+}
+
+console.log(sumWhile(testNumList));
+
+function sumRecursion(numList, index) {
+  if (index >= numList.length) {
+    return 0;
+  }
+  return numList[index] + sumRecursion(numList, index + 1);
+}
+
+console.log(sumRecursion(testNumList, 0));
+
+function sumTheSimpleWay(numList) {
+  return _.reduce(numList, function (memo, num) { return memo + num; }, 0);
+}
+
+console.log(sumTheSimpleWay(testNumList));
